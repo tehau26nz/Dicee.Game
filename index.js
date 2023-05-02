f417721fd7fe286ccadabd183e0fd2637f624230
TNZ <92765882+tehau26nz@users.noreply.github.com>
2023-05-02 16:10:03 +1200

    Final DOM to display winner's msg

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ function getRandomNumber1(min,max){ // Created a function that returns a random 
     return Math.floor(Math.random() *(max - min + 1) + min); // Calculate between min and max and return value inclusive of 1 and 6
 }
 
-console.log(randomNumber1); // Console log to check
-
 
 /********DOM, getters and setters attributes for PL1 and PL2********/
 
@@ -16,3 +14,22 @@ document.querySelector("img.img1").getAttribute("src");
 document.querySelector("img.img1").setAttribute("src","/images/dice"+randomNumber1+".png"); // Switch between the img and number
 document.querySelector("img.img2").getAttribute("src");
 document.querySelector("img.img2").setAttribute("src","/images/dice"+randomNumber2+".png"); // Switch between the img and number
+
+
+/********If condition that changes the title to display a winner********/
+function winningPhrase(){
+    if(randomNumber1<randomNumber2){
+        return "Player 2 wins";
+    }
+    else if(randomNumber1>randomNumber2){
+        return "Player 1 wins";
+    }
+    else if(randomNumber1===randomNumber2){
+        return "Draw";
+    }
+}
+winningPhrase();
+
+
+/********Display the correct msg********/
+document.querySelector("h1").innerHTML = winningPhrase();
