d82bfd2a01e6067cf5b4051fa79ab88572577fb4
TNZ <92765882+tehau26nz@users.noreply.github.com>
2023-05-02 14:49:27 +1200

    Create a random number in JS

---
+++ index.js
@@ -0,0 +1,9 @@
+var randomNumber1 = getRandomNumber1(); // Created the random number variable
+
+function getRandomNumber1(min,max){ // Created a function that returns a random number
+    min = Math.ceil(1); // Min number is 1
+    max = Math.floor(6); // Max number is 6
+    return Math.floor(Math.random() *(max - min + 1) + min); // Calculate between min and max and return value inclusive of 1 and 6
+}
+
+console.log(randomNumber1); // Console log to check
